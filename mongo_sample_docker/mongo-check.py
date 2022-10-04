dbe1ce522b25acd2c23c7a3db1f8ccfc1c2f98e0
Nandha <nandha1712@gmail.com>
2022-10-04 17:24:28 +0530

    added timeseries aggregation queries

--- mongo_sample_docker/mongo-check.py
+++ mongo_sample_docker/mongo-check.py
@@ -5,3 +5,43 @@ print(qualdodb)
 
 collections = qualdodb.list_collection_names()
 print(collections)
+
+dowJonesTickerData = qualdodb.get_collection("dowJonesTickerData")
+
+max_month_close = dowJonesTickerData.aggregate([
+    ## stage 1
+    {
+        "$group" : 
+                 {
+				 "_id" : {"timeunit" : { "$dateTrunc": { "date": "$date", "unit": "month" } }, "symbol": "$symbol" },
+				 "maxMonthClose": { "$max": "$close" }
+				 }
+    },
+
+])
+
+
+print(max_month_close)
+
+for i in max_month_close:
+    print(i)
+
+print("\n\n\n\n========Next set of queries============\n\n\n\n\n\n\n")
+
+avg_hour_close = dowJonesTickerData.aggregate([
+    ## stage 1
+    {
+        "$group" : 
+                 {
+				 "_id" : {"timeunit" : { "$dateTrunc": { "date": "$date", "unit": "hour" } }, "symbol": "$symbol" },
+				 "avgHourClose": { "$avg": "$close" }
+				 }
+    },
+
+])
+
+
+print(avg_hour_close)
+
+for i in avg_hour_close:
+    print(i)
