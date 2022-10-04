9e195881633b7ce9e870717d5b9d7bde5eb0512f
Nandha <nandha1712@gmail.com>
2022-10-05 01:20:29 +0530

    added file for list col

---
+++ mongo_sample_docker/list_col.py
@@ -0,0 +1,9 @@
+import pymongo
+from datetime import datetime
+
+client = pymongo.MongoClient("mongodb://qualdoadmin:12345678@mongodb-service:27017")
+qualdodb = client["qualdo"]
+print(qualdodb)
+
+collections = qualdodb.list_collection_names()
+print(collections)
