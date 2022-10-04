a870c6e35e6f6eca84cb27281b19e2eaef078ce1
Nandha <nandha1712@gmail.com>
2022-10-04 12:29:41 +0530

    added code for mongoclient check dockers

---
+++ mongo_sample_docker/sample_app.py
@@ -0,0 +1,19 @@
+from flask import Flask, session, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/check')
+def hellow(req_id, custom_msg):
+    n_msg = {"msg": "Hello world"}
+    return jsonify(n_msg), 200
+
+
+@app.route('/update/<req_id>/<custom_msg>')
+def post_custom_msg(req_id, custom_msg):
+    n_msg = {"user": "New", "msg": custom_msg, "req_id": req_id}
+    return jsonify(n_msg), 200
+
+
+# at the bottom of the file, use this to run the app
+if __name__ == '__main__':
+    app.run(debug=True)
